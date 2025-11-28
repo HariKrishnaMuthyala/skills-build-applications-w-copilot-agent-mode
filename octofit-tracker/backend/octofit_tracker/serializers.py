399b7614a05f8353a5ae4fa45c40d836f19065d1
HariKrishna Muthyala <harimksp007@yahoo.com>
2025-11-28 18:10:46 +0000

    Fix ObjectId serialization for all models and leaderboards endpoint

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -1,27 +1,41 @@
 from rest_framework import serializers
 from .models import User, Team, Activity, Workout, Leaderboard
 
+
 class TeamSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
     class Meta:
         model = Team
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    team = serializers.PrimaryKeyRelatedField(queryset=Team.objects.all())
     class Meta:
         model = User
         fields = '__all__'
 
+
 class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
         model = Activity
         fields = '__all__'
 
+
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
     class Meta:
         model = Workout
         fields = '__all__'
 
+
+
 class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(read_only=True)
+    user = serializers.CharField(source='user.id', read_only=True)
     class Meta:
         model = Leaderboard
         fields = '__all__'
